1bf5d8b3ec1bedffceac40c0721d93c9aeca41db
聂真 <>
2023-11-22 11:47:10 +0800

    更新

--- src/main/java/com/example/comic/ComicLocalApplication.java
+++ src/main/java/com/example/comic/ComicLocalApplication.java
@@ -1,4 +1,4 @@
-package com.example.comiclocal;
+package com.example.comic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
